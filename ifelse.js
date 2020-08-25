f8362962e63929f970f19f9fbd0894197aa5243b
ayikfaris <farisseriezt@yahoo.com>
2020-08-25 17:45:40 +0700

    episode 22 if else

--- ifelse.js
+++ ifelse.js
@@ -1,12 +1,16 @@
 var input = prompt('Masukkan Nilai : ');
 var jumlahAngkot = 10;
 var operasi = 6;
+var lembur = 8;
 var tidakOperasi = jumlahAngkot - operasi;
 
-if (input < operasi) {
+if (input <= operasi) {
     alert('Angkot no. ' + input + ' beroperasi');
 } else if (input > jumlahAngkot) {
     alert('Jumlah angkot anda hanya ' + jumlahAngkot);
-} else {
+} else if (input == lembur) {
+    alert('Angkot no. ' + input + ' lembur');
+}
+else {
     alert('Angkot no. ' + input + ' tidak beroperasi');
 }
