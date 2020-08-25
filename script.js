87e04a7a88c3ab4046b614005f865090809775b0
ayikfaris <farisseriezt@yahoo.com>
2020-08-25 17:07:36 +0700

    episode 17 control flow

--- script.js
+++ script.js
@@ -8,14 +8,33 @@
 
 
 //confirm
-alert('Selamat datang..');
-var lagi = true;
+// alert('Selamat datang..');
+// var lagi = true;
 
-while (lagi) {
-    var nama = prompt('masukkan nama : ');
-    alert('halo ' + nama);
 
-    lagi = confirm('coba lagi?');
-}
+// while (lagi) {
+//     var nama = prompt('masukkan nama : ');
+//     alert('halo ' + nama);
 
-alert('terimakasih');
+//     lagi = confirm('coba lagi?');
+// }
+
+// alert('terimakasih');
+
+
+//-----------------//
+//17. Control flow
+// for
+// alert('Mulai');
+// for (var i = 1; i <= 5; i++) {
+//     alert('Hello World ' + i);
+// }
+// alert('Selesai');
+
+//if
+// var angka = prompt('Masukkan angka : ');
+// if (angka % 2 === 0) {
+//     alert(angka + ' merupakan bilangan GENAP');
+// } else {
+//     alert(angka + ' merupakan bilangan GANJIL');
+// }
